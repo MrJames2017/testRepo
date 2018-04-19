fe56ec2c377ca6ac88940939b647a2a8517ac721
james <32944470+MrJames2017@users.noreply.github.com>
2018-04-19 13:46:40 -0400

    Added a function to display length of name

--- test.py
+++ test.py
@@ -3,5 +3,7 @@ def hello(name="user"):
 	print("Hello {}".format(name))
 def goodbye(name="user"):
 	print("Goodbye {}".format(name))
+def nameLength(name="user"):
+	print("Your name is {} characters long".format(len(name)))
 if __name__ =="__main__":
 	hello()
--- test2.py
+++ test2.py
@@ -2,4 +2,7 @@
 import test
 name = input("What is your name\n>")
 test.hello(name)
+test.nameLength(name)
+print("\n")
 test.goodbye(name)
+
