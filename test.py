59011664f276d5bfed8851f5d28262aa41266ffc
MrJames2017 <32944470+MrJames2017@users.noreply.github.com>
2018-04-19 13:59:56 -0400

    added reverse name

--- test.py
+++ test.py
@@ -5,5 +5,7 @@ def goodbye(name="user"):
 	print("Goodbye {}".format(name))
 def nameLength(name="user"):
 	print("Your name is {} characters long".format(len(name)))
+def backwards(name="user"):
+	print("{} is how you spell your name backwards".format(name[::-1]))
 if __name__ =="__main__":
 	hello()
--- test2.py
+++ test2.py
@@ -3,6 +3,7 @@ import test
 name = input("What is your name\n>")
 test.hello(name)
 test.nameLength(name)
+test.backwards(name)
 print("\n")
 test.goodbye(name)
 
